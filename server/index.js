7ae3ef803f8ea5fe69c9886597e841396125c2f6
Manik Rana <manikrana54@gmail.com>
2023-04-13 10:20:33 +0530

    feat: add cors for messages route

--- server/index.js
+++ server/index.js
@@ -72,3 +72,10 @@ io.on("connection", (socket) => {
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
